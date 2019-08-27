c32a45149f4d7ca4b336915963bdbcc284a2ce87
Jonn <nojj02@gmail.com>
2019-08-28 00:22:01 +0800

    Does not allow negatives

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,34 @@
-function add(stringInput : string) : Number {
+interface Left<A> {
+    value: A;
+    tag: 'left'
+}
+
+interface Right<B> {
+    value: B;
+    tag: 'right'
+}
+
+type Either<A, B> = Left<A> | Right<B>;
+
+function isLeft<A>(val: any): val is Left<A> {
+    if ((val as Left<A>).tag === 'left') return true;
+    return false;
+}
+
+function isRight<B>(val: any): val is Right<B> {
+    if ((val as Right<B>).tag === 'right') return true;
+    return false;
+}
+
+function Left<A>(val: A): Left<A> {
+    return { value: val, tag: 'left' };
+}
+
+function Right<B>(val: B): Right<B> {
+    return { value: val, tag: 'right' };
+}
+
+function add(stringInput: string): Number {
     if (stringInput.length === 0) {
         return 0;
     } else {
@@ -12,18 +42,43 @@ function add(stringInput : string) : Number {
         }
 
         return splitByDelimiters([equation], delimiters)
-            .map((splittedString : string) => parseInt(splittedString))
-            .reduce((previousValue : number, currentValue : number) => previousValue + currentValue, 0);
+            .map((splittedString: string) => parseInt(splittedString))
+            .reduce((previousValue: number, currentValue: number) => previousValue + currentValue, 0);
     }
 }
 
-function splitByDelimiters(splittableNumberStrings : string[], delimiters : string[]) : string[] {
-    return delimiters
-        .reduce((splittedNumberStrings : string[], currentDelimiter : string) => splitByDelimiter(splittedNumberStrings, currentDelimiter), splittableNumberStrings);
+function add2(stringInput: string): Either<Error, Number> {
+    if (stringInput.length === 0) {
+        return Right(0);
+    } else {
+        const delimiters = [",", "\n"]
+
+        let equation = stringInput;
+        if (stringInput.startsWith("//")) {
+            const newDelimiter = stringInput.charAt(2);
+            equation = stringInput.substr(4, stringInput.length - 4);
+            delimiters.push(newDelimiter);
+        }
+
+        const numbers =
+            splitByDelimiters([equation], delimiters)
+                .map((splittedString: string) => parseInt(splittedString));
+        
+        if (numbers.some(number => number < 0)) {
+            return Left(new Error("No negatives allowed"));
+        }
+        const sum = numbers.reduce((previousValue: number, currentValue: number) => previousValue + currentValue, 0);
+        return Right(sum);
+    }
 }
 
-function splitByDelimiter(splittableNumberStrings : string[], delimiter : string) : string[] {
-    return splittableNumberStrings.flatMap((splittedString : string) => splittedString.split(delimiter));
+function splitByDelimiters(splittableNumberStrings: string[], delimiters: string[]): string[] {
+    return delimiters
+        .reduce((splittedNumberStrings: string[], currentDelimiter: string) => splitByDelimiter(splittedNumberStrings, currentDelimiter), splittableNumberStrings);
+}
+
+function splitByDelimiter(splittableNumberStrings: string[], delimiter: string): string[] {
+    return splittableNumberStrings.flatMap((splittedString: string) => splittedString.split(delimiter));
 }
 
 test("Adds nothing; Returns 0", function () {
@@ -51,12 +106,18 @@ test("Adds three numbers; Returns sum", function () {
     expect(result).toBe(15);
 });
 
-test("Handle newlines between numbers", function() {
+test("Handle newlines between numbers", function () {
     const result = add("1\n2,3");
     expect(result).toBe(6);
 });
 
-test("Support different delimiters", function() {
+test("Support different delimiters", function () {
     const result = add("//;\n1;2");
     expect(result).toBe(3);
+});
+
+test("No negatives allowed", function () {
+    const result = add2("//;\n1;-2");
+    expect(isLeft(result)).toBeTruthy();
+    expect((result.value as Error).message).toBe('No negatives allowed');
 });
