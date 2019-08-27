e5f341b1da55c3956b2611b79fe107c58a39f3dc
Jonn <nojj02@gmail.com>
2019-08-28 00:27:16 +0800

    Removed old class completely

--- src/test.ts
+++ src/test.ts
@@ -28,26 +28,7 @@ function Right<B>(val: B): Right<B> {
     return { value: val, tag: 'right' };
 }
 
-function add(stringInput: string): Number {
-    if (stringInput.length === 0) {
-        return 0;
-    } else {
-        const delimiters = [",", "\n"]
-
-        let equation = stringInput;
-        if (stringInput.startsWith("//")) {
-            const newDelimiter = stringInput.charAt(2);
-            equation = stringInput.substr(4, stringInput.length - 4);
-            delimiters.push(newDelimiter);
-        }
-
-        return splitByDelimiters([equation], delimiters)
-            .map((splittedString: string) => parseInt(splittedString))
-            .reduce((previousValue: number, currentValue: number) => previousValue + currentValue, 0);
-    }
-}
-
-function add2(stringInput: string): Either<Error, Number> {
+function add(stringInput: string): Either<Error, Number> {
     if (stringInput.length === 0) {
         return Right(0);
     } else {
@@ -83,41 +64,48 @@ function splitByDelimiter(splittableNumberStrings: string[], delimiter: string):
 
 test("Adds nothing; Returns 0", function () {
     const result = add("");
-    expect(result).toBe(0);
+    expect(isRight(result)).toBeTruthy();
+    expect(result.value).toBe(0);
 });
 
 test("Adds single number; Returns same number", function () {
     const result = add("1");
-    expect(result).toBe(1);
+    expect(isRight(result)).toBeTruthy();
+    expect(result.value).toBe(1);
 });
 
 test("Adds another single number; Returns same number", function () {
     const result = add("2");
-    expect(result).toBe(2);
+    expect(isRight(result)).toBeTruthy();
+    expect(result.value).toBe(2);
 });
 
 test("Adds two numbers; Returns sum", function () {
     const result = add("1,2");
-    expect(result).toBe(3);
+    expect(isRight(result)).toBeTruthy();
+    expect(result.value).toBe(3);
 });
 
 test("Adds three numbers; Returns sum", function () {
     const result = add("1,2,3,4,5");
-    expect(result).toBe(15);
+    expect(isRight(result)).toBeTruthy();
+    expect(result.value).toBe(15);
 });
 
 test("Handle newlines between numbers", function () {
     const result = add("1\n2,3");
-    expect(result).toBe(6);
+    expect(isRight(result)).toBeTruthy();
+    expect(result.value).toBe(6);
 });
 
 test("Support different delimiters", function () {
     const result = add("//;\n1;2");
-    expect(result).toBe(3);
+    expect(isRight(result)).toBeTruthy();
+    expect(result.value).toBe(3);
 });
 
 test("No negatives allowed", function () {
-    const result = add2("//;\n1;-2");
+    const result = add("//;\n1;-2");
     expect(isLeft(result)).toBeTruthy();
     expect((result.value as Error).message).toBe('No negatives allowed');
 });
